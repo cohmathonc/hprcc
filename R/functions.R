92bc751ef1bd1ab61ebdc19cccd014f85de9a381
Denis O'Meally <domeally@coh.org>
2024-12-12 22:59:02 +0000

    multiple fixes

--- R/functions.R
+++ R/functions.R
@@ -6,25 +6,41 @@
 #' Package Options for hprcc
 #'
 #' The **hprcc** package has a number of settings that can be configured
-#' via \code{\link[base]{options}} or environment variables, providing 
+#' via \code{\link[base]{options}} or environment variables, providing
 #' the flexibility to use it with any containerized environment supporting
 #' R and `{targets>=1.9.1}`.
 #'
 #' Options can be set by calling \code{\link[base]{options}} _before_ loading the **hprcc** package in
-#' `_targets.R`. Option settings take precedence over environment variables, where 
+#' `_targets.R`. Option settings take precedence over environment variables, where
 #' indicated below. If no `options` are set, the default configuration
 #' runs the [RStudio for Bioconductor](http://hprcc.coh.org/user-guide/rbioc/) container.
 #'
 #' @section Options:
-#' - \code{hprcc.slurm_log}: Enable SLURM job & autometric logging. If `TRUE`, logs are saved in the `logs` folder within \code{tar_store_path()}. Logs capture the `stderr` and `stdout` of each SLURM job, and can be parsed by code\{autometric} package. \cr Default: \code{FALSE}.
-#' - \code{hprcc.slurm_verbose}: Show slurm messages in the console. \cr Default: \code{FALSE}
-#' - \code{hprcc.slurm_script_dir}: A subfolder of \code{tar_store_path()} in which to stage SLURM job scripts. \cr Default: \code{tempdir()}
-#' - \code{hprcc.r_libs_user}: Path to user R libraries. \cr If not set, defaults to \code{R_LIBS_SITE} environment variable or none.
-#' - \code{hprcc.r_libs_site}: Site-specific library path. \cr Default set by \code{R_LIBS_USER} \cr Apollo default: \code{"/opt/singularity-images/rbioc/rlibs/bioc-VERSION"} \cr Gemini default: \code{"/packages/singularity/shared_cache/rbioc/rlibs/bioc-VERSION"}
-#' - \code{hprcc.singularity_bin}: Path to the Singularity binary. \cr Apollo default: \code{"/opt/singularity/3.7.0/bin/singularity"} \cr  Gemini default: \code{"/packages/easy-build/software/singularity/3.7.0/bin/singularity"}
-#' - \code{hprcc.singularity_container}: Path to the Singularity image. \cr Default set by \code{SINGULARITY_CONTAINER} \cr Apollo default: \code{"/opt/singularity-images/rbioc/vscode-rbioc_VERSION.sif"} \cr Gemini default: \code{"/packages/singularity/shared_cache/rbioc/vscode-rbioc_VERSION.sif"}
-#' - \code{hprcc.bind_dirs}: Directories to bind in the Singularity container. \cr Default set by \code{SINGULARITY_BIND} \cr Apollo default: \code{"/labs,/opt,/ref_genome"} \cr Gemini default: \code{"/packages/singularity,/ref_genomes,/scratch"}
-#' - \code{hprcc.default_partition}: Default SLURM partition. \cr Apollo default: \code{"all"} \cr Gemini default: \code{"compute"}
+#' - \code{hprcc.slurm_logs}: logical. Enable SLURM job & autometric logging. If `TRUE`, logs are saved to
+#'   \code{tar_store_path()/logs}. Logs capture the `stderr` and `stdout` of each SLURM job, and can be parsed by
+#'   code\{autometric} package. \cr Default: \code{FALSE}.
+#' - \code{hprcc.slurm_verbose}: logical. Show SLURM messages in the console. \cr Default: \code{FALSE}
+#' - \code{hprcc.slurm_jobs}: logical. Write SLURM submission scripts to \code{tar_store_path()/jobs}; use the
+#'   `targets` default of `$TMPDIR` if `FALSE` \cr Default: `TRUE`
+#' - \code{hprcc.r_libs_user}: Path to user R libraries. \cr If not set, defaults to \code{R_LIBS_SITE} environment
+#'   variable or the R default of "~/R/x86_64-pc-linux-gnu-library/%V".
+#' - \code{hprcc.r_libs_site}: Site-specific library path. \cr Default set by \code{R_LIBS_USER} \cr
+#'   Apollo default: \code{"/opt/singularity-images/rbioc/rlibs/bioc-VERSION"} \cr
+#'   Gemini default: \code{"/packages/singularity/shared_cache/rbioc/rlibs/bioc-VERSION"}
+#' - \code{hprcc.singularity_bin}: Path to the Singularity binary. \cr
+#'   Apollo default: \code{"/opt/singularity/3.7.0/bin/singularity"} \cr
+#'   Gemini default: \code{"/packages/easy-build/software/singularity/3.7.0/bin/singularity"}
+#' - \code{hprcc.singularity_container}: Path to the Singularity image. \cr
+#'   Default set by \code{SINGULARITY_CONTAINER} \cr
+#'   Apollo default: \code{"/opt/singularity-images/rbioc/vscode-rbioc_VERSION.sif"} \cr
+#'   Gemini default: \code{"/packages/singularity/shared_cache/rbioc/vscode-rbioc_VERSION.sif"}
+#' - \code{hprcc.bind_dirs}: Directories to bind in the Singularity container. \cr
+#'   Default set by \code{SINGULARITY_BIND} \cr
+#'   Apollo default: \code{"/labs,/opt,/ref_genome"} \cr
+#'   Gemini default: \code{"/packages/singularity,/ref_genomes,/scratch"}
+#' - \code{hprcc.default_partition}: Default SLURM partition. \cr
+#'   Apollo default: \code{"all"} \cr
+#'   Gemini default: \code{"compute"}
 #'
 #' @name package-options
 #' @aliases hprcc-package
@@ -66,9 +82,8 @@ get_cluster <- function() {
 #' @param slurm_mem_gigabytes Memory allocated to each task, in gigabytes.
 #' @param slurm_walltime_minutes Maximum allowed execution time per task, in minutes. Defaults to 720 (12 hours).
 #' @param slurm_workers Total number of parallel tasks the controller can handle. Defaults to 350.
-#' @param slurm_partition SLURM partition for job submission. Default set by cluster. See [package options](../reference/package-options.html) for defaults.
-#' @param slurm_log_dir Relative path for saving SLURM & autometric logs when `option(hprcc.log_slurm = TRUE)`. Defaults to "logs" in the targets directory.
-#' @param slurm_script_dir Defaults to tempdir(), but can also be a relative path to save SLURM job files (for debugging.)
+#' @param slurm_partition SLURM partition for job submission. Default set by cluster.
+#' See [package options](../reference/package-options.html) for defaults.
 #'
 #' @details
 #' `create_controller` streamlines SLURM job setup on COH clusters using
@@ -98,16 +113,13 @@ create_controller <- function(name,
                               slurm_mem_gigabytes,
                               slurm_walltime_minutes = 720L,
                               slurm_workers = 350L,
-                              slurm_partition = default_partition(),
-                              slurm_log_dir = "logs",
-                              slurm_script_dir = tempdir()) { #default
-#                              slurm_script_dir = "job_scripts") { # debugging  
+                              slurm_partition = default_partition()) {
     # GPU check
     if (grepl("gpu", slurm_partition)) {
         if (get_cluster() != "gemini") {
             stop("GPU jobs are only supported on the Gemini cluster.")
         }
-        if (slurm_cpus > 8) {
+        if (slurm_cpus > 8L) {
             stop("For GPU partitions, the number of CPUs must be less than or equal to 8.")
         }
         gpu_req <- glue::glue("#SBATCH --gres gpu:1 \n#SBATCH --ntasks=1 \n")
@@ -115,21 +127,14 @@ create_controller <- function(name,
         gpu_req <- ""
     }
 
-    job_id <- Sys.getenv("SLURM_JOB_ID")
-    
     nodename <- Sys.info()["nodename"]
 
-    r_libs_user <- if (nzchar(r_libs_user <- getOption("hprcc.r_libs_user", Sys.getenv("R_LIBS_USER")))) glue::glue("--env R_LIBS_USER={r_libs_user} \\") else ""
-
-    r_libs_site <- r_libs_site()
-
-    slurm_script_dir <- if (slurm_script_dir != tempdir()) {
-        here::here(glue::glue("{targets::tar_path_store()}/{slurm_script_dir}"))
+    r_libs_user <- if (nzchar(user_libs_path <- getOption("hprcc.r_libs_user", Sys.getenv("R_LIBS_USER")))) {
+        glue::glue("--env R_LIBS_USER={user_libs_path}")
     } else {
-        tempdir()
+        ""
     }
-
-    slurm_log_dir <- here::here(glue::glue("{targets::tar_path_store()}/{slurm_log_dir}"))
+    r_libs_site <- r_libs_site()
 
     slurm_account <- if (nzchar(account <- getOption("hprcc.slurm_account", ""))) glue::glue("#SBATCH --account {account}") else ""
 
@@ -139,47 +144,49 @@ create_controller <- function(name,
 
     singularity_container <- singularity_container()
 
-    log_slurm <- getOption("hprcc.slurm_log", FALSE)
+    use_jobs_dir <- isTRUE(getOption("hprcc.slurm_jobs", TRUE))
+    slurm_jobs_dir <- if (use_jobs_dir) here::here(glue::glue("{targets::tar_path_store()}/jobs")) else NULL
+    if (use_jobs_dir) dir.create(slurm_jobs_dir, recursive = TRUE, showWarnings = FALSE)
+
+    use_slurm_log <- isTRUE(getOption("hprcc.slurm_logs", FALSE))
+    log_output <- here::here(glue::glue("{targets::tar_path_store()}/logs/slurm-%j.out"))
+    if (use_slurm_log) {
+        dir.create(dirname(log_output), recursive = TRUE, showWarnings = FALSE)
+    } else {
+        log_output <- "/dev/null"
+    }
 
     verbose_slurm <- getOption("hprcc.slurm_verbose", FALSE)
 
-    if (isTRUE(log_slurm)) dir.create(slurm_log_dir, showWarnings = FALSE, recursive = TRUE)
-    log_output <- if (isTRUE(log_slurm)) glue::glue("{slurm_log_dir}/slurm-%j.out") else "/dev/null"
-
-    if (slurm_script_dir != tempdir()) {
-        dir.create(here::here(slurm_script_dir), showWarnings = FALSE, recursive = TRUE)
-        slurm_script_dir <- here::here(slurm_script_dir)
-    }
-
-    script_lines <- glue::glue("#SBATCH --chdir {slurm_script_dir} \
-{gpu_req} \
-{slurm_account} \
-{singularity_bin} exec \\
-{r_libs_user} \\
+    script_lines <- glue::glue(
+    "{if (!is.null(gpu_req) && nzchar(gpu_req)) gpu_req else ''} ",
+    "{if (!is.null(slurm_account) && nzchar(slurm_account)) slurm_account else ''} ",
+    "{singularity_bin} exec {if (!is.null(r_libs_user) && nzchar(r_libs_user)) r_libs_user else ''} \\
 --env R_LIBS_SITE={r_libs_site} \\
+--env R_PARALLELLY_AVAILABLECORES_METHODS=Slurm \\
 -B {singularity_bind_dirs} \\
 {singularity_container} \\")
 
-slurm_options <- crew.cluster::crew_options_slurm(
-    script_directory = slurm_script_dir,
-    script_lines = script_lines,
-    cpus_per_task = slurm_cpus,
-    memory_gigabytes_required = slurm_mem_gigabytes,
-    time_minutes = slurm_walltime_minutes,
-    partition = slurm_partition,
-    log_output = log_output,
-    log_error = log_output,
-    verbose = ifelse(isTRUE(verbose_slurm), TRUE, FALSE)
-)
+    slurm_options <- crew.cluster::crew_options_slurm(
+        script_directory = slurm_jobs_dir,
+        script_lines = script_lines,
+        cpus_per_task = slurm_cpus,
+        memory_gigabytes_required = slurm_mem_gigabytes,
+        time_minutes = slurm_walltime_minutes,
+        partition = slurm_partition,
+        log_output = log_output,
+        log_error = log_output,
+        verbose = isTRUE(verbose_slurm)
+    )
 
     crew.cluster::crew_controller_slurm(
         name = name,
         host = nodename,
         workers = slurm_workers,
-        seconds_idle = 30,
+        seconds_idle = 30L,
         garbage_collection = TRUE,
         options_cluster = slurm_options,
-        options_metrics = crew::crew_options_metrics(path = "/dev/stdout", seconds_interval = 1)
+        options_metrics = crew::crew_options_metrics(path = "/dev/stdout", seconds_interval = 1L)
     )
 }
 
@@ -191,9 +198,9 @@ r_libs_site <- function() {
     } else if (nzchar(Sys.getenv("R_LIBS_SITE"))) {
         return(Sys.getenv("R_LIBS_SITE"))
     } else if (get_cluster() == "apollo") {
-        return(glue::glue("/opt/singularity-images/rbioc/rlibs/bioc-", Sys.getenv("BIOCONDUCTOR_VERSION")))
+        return(glue::glue("/opt/singularity-images/rbioc/rlibs/bioc-{Sys.getenv('BIOCONDUCTOR_VERSION')}"))
     } else if (get_cluster() == "gemini") {
-        return(glue::glue("/packages/singularity/shared_cache/rbioc/rlibs/bioc-", Sys.getenv("BIOCONDUCTOR_VERSION")))
+        return(glue::glue("/packages/singularity/shared_cache/rbioc/rlibs/bioc-{Sys.getenv('BIOCONDUCTOR_VERSION')}"))
     } else {
         warning("Unknown cluster, please set R_LIBS_SITE env var or option")
     }
@@ -221,7 +228,7 @@ singularity_container <- function() {
     } else if (get_cluster() == "apollo") {
         return(glue::glue("/opt/singularity-images/rbioc/vscode-rbioc_", Sys.getenv("BIOCONDUCTOR_VERSION"), ".sif"))
     } else if (get_cluster() == "gemini") {
-        return(glue::glue("/packages/singularity/shared_cache/rbioc/vscode-rbioc_", Sys.getenv("BIOCONDUCTOR_VERSION"), ".sif"))
+        return(glue::glue("/packages/singularity/shared_cache/rbioc/vscode-rbioc_{Sys.getenv('BIOCONDUCTOR_VERSION')}.sif"))
     } else {
         warning("Unknown cluster, please set SINGULARITY_CONTAINER env var or option")
     }
@@ -233,42 +240,33 @@ singularity_bind_dirs <- function() {
     } else if (nzchar(Sys.getenv("SINGULARITY_BIND"))) {
         return(Sys.getenv("SINGULARITY_BIND"))
     } else if (get_cluster() == "apollo") {
-        return(c("/labs,/opt,/ref_genome,/run"))
+        return("/labs,/opt,/ref_genome,/run")
     } else if (get_cluster() == "gemini") {
-        return(c("/packages,/run,/ref_genomes,/scratch"))
+        return("/packages,/run,/ref_genomes,/scratch")
     } else {
         warning("Unknown cluster, please set SINGULARITY_BIND env var or option")
     }
 }
 
-
 slurm_default_partition <- function() {
-    # Make the system call and capture output
     cmd_output <- system("scontrol show partition", intern = TRUE)
-
-    # Initialize variables
     current_partition <- NULL
-
-    # Process each line
+    # Process each line 
     for (i in seq_along(cmd_output)) {
         line <- cmd_output[i]
-
         # If line starts with PartitionName, get the partition name
         if (grepl("^PartitionName=", line)) {
             current_partition <- sub("PartitionName=([^ ]+).*", "\\1", line)
-        }
-        # If we find Default=YES in a line and we have a current partition
-        else if (!is.null(current_partition) && grepl("Default=YES", line)) {
+        } else if (!is.null(current_partition) && grepl("Default=YES", line)) {
             return(current_partition)
         }
     }
-
     # Return NULL if no default partition found
     return(NULL)
 }
 
 default_partition <- function() {
-    # Check if there's a user-specified default partition in options
+    # Check for partition in options
     if (!is.null(getOption("hprcc.default_partition"))) {
         return(getOption("hprcc.default_partition"))
     } else {
@@ -284,20 +282,24 @@ default_partition <- function() {
 }
 
 # Default Targets options ----------------------------------------------------
-#' @importFrom targets tar_option_set
-#' @importFrom targets tar_resources
-#' @importFrom crew crew_controller_group
 configure_targets_options <- function() {
     # Define the common controllers
     controllers <- list(
-        create_controller("tiny", slurm_cpus = 2, slurm_mem_gigabytes = 8, slurm_walltime_minutes = 60),
-        create_controller("small", slurm_cpus = 2, slurm_mem_gigabytes = 20, slurm_walltime_minutes = 360),
-        create_controller("medium", slurm_cpus = 4, slurm_mem_gigabytes = 40, slurm_walltime_minutes = 360),
-        create_controller("large", slurm_cpus = 8, slurm_mem_gigabytes = 80, slurm_walltime_minutes = 360),
-        create_controller("large_mem", slurm_cpus = 8, slurm_mem_gigabytes = 800, slurm_walltime_minutes = 360, slurm_partition = ifelse(get_cluster() == "apollo", "all", "bigmem")),
-        create_controller("xlarge", slurm_cpus = 20, slurm_mem_gigabytes = 200),
-        create_controller("huge", slurm_cpus = 40, slurm_mem_gigabytes = 200, slurm_walltime_minutes = 120),
-        create_controller("retry", slurm_cpus = c(2, 4, 8, 20, 40), slurm_mem_gigabytes = c(8,20,40,80,120,200), slurm_walltime_minutes = c(60,360,360,720,720))
+        create_controller("tiny", slurm_cpus = 2L, slurm_mem_gigabytes = 8L, slurm_walltime_minutes = 60L),
+        create_controller("small", slurm_cpus = 2L, slurm_mem_gigabytes = 20L, slurm_walltime_minutes = 360L),
+        create_controller("medium", slurm_cpus = 4L, slurm_mem_gigabytes = 40L, slurm_walltime_minutes = 360L),
+        create_controller("large", slurm_cpus = 8L, slurm_mem_gigabytes = 80L, slurm_walltime_minutes = 360L),
+        create_controller("large_mem",
+            slurm_cpus = 8L, slurm_mem_gigabytes = 800L, slurm_walltime_minutes = 360L,
+            slurm_partition = ifelse(get_cluster() == "apollo", "all", "bigmem")
+        ),
+        create_controller("xlarge", slurm_cpus = 20L, slurm_mem_gigabytes = 200L),
+        create_controller("huge", slurm_cpus = 40L, slurm_mem_gigabytes = 200L, slurm_walltime_minutes = 120L),
+        create_controller("retry",
+            slurm_cpus = c(2L, 4L, 8L, 20L, 40L),
+            slurm_mem_gigabytes = c(8L, 20L, 40L, 80L, 120L, 200L),
+            slurm_walltime_minutes = c(60L, 360L, 360L, 720L, 720L)
+        )
     )
 
     # Conditionally add GPU controllers if on the 'gemini' cluster
@@ -327,17 +329,11 @@ configure_targets_options <- function() {
     configure_targets_options()
     # Set parallelly options
     if (nzchar(Sys.getenv("SLURM_JOB_ID"))) options(parallelly.availableCores.methods = "Slurm")
-    # set TMPDIR
-    if (!is.null(getOption("hprcc.tmpdir"))) { 
-        Sys.setenv(TMPDIR = getOption("hprcc.tmpdir"))
-        dir.create(getOption("hprcc.tmpdir"), recursive = TRUE, showWarnings = FALSE)
-        message("TMPDIR set to ", Sys.getenv("TMPDIR"))
-    } else {
-        message("TMPDIR set to ", Sys.getenv("TMPDIR"))
-    }
+    # log hprcc settings to logs/hprcc_settings.txt if option(hprcc.slurm_logs = TRUE)
+    log_hprcc_settings()
 }
 
 .onLoad <- function(libname, pkgname) {
     # Set parallelly options
-      if (nzchar(Sys.getenv("SLURM_JOB_ID"))) options(parallelly.availableCores.methods = "Slurm")
+    if (nzchar(Sys.getenv("SLURM_JOB_ID"))) options(parallelly.availableCores.methods = "Slurm")
 }
