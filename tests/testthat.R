ccc4162700055a0e56744cb0cfc94cfc1a416f73
Denis O'Meally <omeally@gmail.com>
2024-12-12 20:44:37 -0700

    last passing tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,27 +1,11 @@
-# Debug prints
-cat("Starting test setup...\n")
+# This file is part of the standard setup for testthat.
+# It is recommended that you do not modify it.
+#
+# Where should you do additional test configuration?
+# Learn more about the roles of various files in:
+# * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
+# * https://testthat.r-lib.org/articles/special-files.html
 
-# Basic setup
 library(testthat)
-library(mockery)
 
-cat("Libraries loaded...\n")
-
-# Prevent SLURM configuration during tests
-Sys.setenv(
-    R_TESTS = "",
-    TESTTHAT = "true",
-    SLURM_JOB_ID = ""
-)
-
-cat("Environment set...\n")
-
-# Basic package options
-options(
-    hprcc.slurm_logs = FALSE,
-    hprcc.slurm_verbose = FALSE
-)
-
-cat("Running tests...\n")
-test_check("hprcc", reporter = "progress")
-cat("Tests complete\n")
+test_check("hprcc")
--- tests/testthat/test-core.R
+++ tests/testthat/test-core.R
@@ -1,27 +1,137 @@
 library(testthat)
-library(mockery)
 
-test_that("default_partition uses option if set", {
-    withr::with_options(
-        new = list(hprcc.default_partition = "test_partition"),
-        code = {
-            expect_equal(default_partition(), "test_partition")
+# Tests for get_cluster()
+test_that("get_cluster returns 'apollo' for matching hostname", {
+    with_mocked_bindings(
+        Sys.info = function() list(nodename = "ppxhpc123"),
+        {
+            expect_equal(get_cluster(), "apollo")
         }
     )
 })
 
-test_that("default_partition handles missing SLURM gracefully", {
-    withr::with_options(
-        new = list(hprcc.default_partition = NULL),
-        code = {
-            # Mock slurm_default_partition to return NULL
-            mockery::stub(default_partition, "slurm_default_partition", NULL)
-
-            expect_warning(
-                partition <- default_partition(),
-                "Could not determine default partition"
-            )
-            expect_null(partition)
+test_that("get_cluster returns 'gemini' for matching hostname", {
+    with_mocked_bindings(
+        Sys.info = function() list(nodename = "g-a-1-2-34"),
+        {
+            expect_equal(get_cluster(), "gemini")
         }
+    )
+})
+
+test_that("get_cluster gives a warning for unknown hostname", {
+    with_mocked_bindings(
+        Sys.info = function() list(nodename = "unknown"),
+        {
+            expect_warning(cluster <- get_cluster(), "Unknown cluster")
+            expect_equal(cluster, "Unknown cluster") # Changed from expect_null
+        }
+    )
+})
+# Tests for slurm_allocation()
+test_that("slurm_allocation retrieves correct SLURM job resources", {
+    mock_sys_getenv <- function(x) {
+        switch(x,
+            "SLURM_JOB_ID" = "123456",
+            "SLURM_CPUS_PER_TASK" = "",
+            "SLURM_CPUS_ON_NODE" = "6",
+            "SLURM_MEM_PER_NODE" = "61440", # 60GB in MB
+            "" # default for any other env var
+        )
+    }
+
+    with_mocked_bindings(
+        Sys.getenv = mock_sys_getenv,
+        code = {
+            allocation <- slurm_allocation()
+            expect_equal(allocation$job_id, "123456")
+            expect_equal(allocation$CPUs, 6)
+            expect_equal(allocation$Memory_GB, 60)
+        }
+    )
+})
+
+test_that("slurm_allocation handles non-SLURM environment", {
+    mock_sys_getenv <- function(x) "" # Return empty string for all env vars
+
+    with_mocked_bindings(
+        Sys.getenv = mock_sys_getenv,
+        code = {
+            expect_warning(allocation <- slurm_allocation(), "SLURM_JOB_ID not set")
+            expect_null(allocation)
+        }
+    )
+})
+
+test_that("slurm_allocation handles partial environment variables", {
+    mock_sys_getenv <- function(x) {
+        switch(x,
+            "SLURM_JOB_ID" = "123456",
+            "SLURM_CPUS_PER_TASK" = "4",
+            "SLURM_CPUS_ON_NODE" = "",
+            "SLURM_MEM_PER_NODE" = "",
+            ""
+        )
+    }
+
+    with_mocked_bindings(
+        Sys.getenv = mock_sys_getenv,
+        code = {
+            expect_warning(allocation <- slurm_allocation(), "SLURM_MEM_PER_NODE not set")
+            expect_equal(allocation$job_id, "123456")
+            expect_equal(allocation$CPUs, 4)
+            expect_null(allocation$Memory_GB)
+        }
+    )
+})
+
+# Tests for init_multisession()
+# Test for SLURM environment
+test_that("init_multisession sets up future plan correctly in SLURM environment", {
+    mock_sys_getenv <- function(...) {
+        args <- list(...)
+        var <- args[[1]]
+        switch(var,
+            "SLURM_JOB_ID" = "123456",
+            "SLURM_CPUS_PER_TASK" = "4",
+            "SLURM_CPUS_ON_NODE" = "",
+            "SLURM_MEM_PER_NODE" = "8192", # 8GB in MB
+            ""
+        )
+    }
+
+    withr::with_options(
+        new = list(future.globals.maxSize = NULL),
+        with_mocked_bindings(
+            Sys.getenv = mock_sys_getenv,
+            code = {
+                init_multisession()
+                expect_true(inherits(future::plan(), "multisession"))
+                expect_equal(
+                    getOption("future.globals.maxSize"),
+                    8 * 1024^3 / 4 # 8GB divided by 4 workers
+                )
+            }
+        )
+    )
+})
+
+# Test for non-SLURM environment
+test_that("init_multisession works correctly outside SLURM environment", {
+    mock_sys_getenv <- function(...) "" # Always return empty string
+    mock_system <- function(...) "16" # 16GB system memory
+
+    withr::with_options(
+        new = list(future.globals.maxSize = NULL),
+        with_mocked_bindings(
+            `Sys.getenv` = mock_sys_getenv,
+            system = mock_system,
+            code = {
+                init_multisession()
+                expect_true(inherits(future::plan(), "multisession"))
+                # In non-SLURM case, future.globals.maxSize isn't set
+                expect_null(getOption("future.globals.maxSize"))
+            }
+        )
     )
 })
